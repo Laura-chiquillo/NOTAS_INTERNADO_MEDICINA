ec3b28ff9cdb08eb76c8acd758c51223558739dc
Laura-Chiquillo <lchiquillo@unbosque.edu.co>
2022-04-11 22:35:24 -0500

    Back y front listos

---
+++ Backend/unbosque/src/test/java/Backend/unbosque/UnbosqueApplicationTests.java
@@ -0,0 +1,13 @@
+package Backend.unbosque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnbosqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
