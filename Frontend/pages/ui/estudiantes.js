1a2f28ecaf8ac54f520c8f424b93e0e217a09adc
Juan David Quintero <slim.shady99q@gmail.com>
2022-05-04 22:09:10 -0500

    waaaaa

--- Frontend/pages/ui/estudiantes.js
+++ Frontend/pages/ui/estudiantes.js
@@ -71,65 +71,90 @@ const Buttons = () => {
         <CardBody>
           <FormGroup>
             <Label for="exampleText"></Label>
-            <Accordion>
-              <Accordion.Item eventKey="0">
-                <Accordion.Header>Jenifer Acosta
-                </Accordion.Header>
-                <Accordion.Body>
-                  <li>Documento: 10000000</li>
-                  <li>semestre actual: 11</li>
-                  <li>Hospital actual: curitas del saber</li>
-                  <li>fecha inicio actual: 21/02/22</li>
-                  <li>Fecha finalización actual: 21/04/2022</li>
-                  <Accordion>
-                    <Accordion.Item eventKey="0">
-                      <Accordion.Header>Rotación 1
-                      </Accordion.Header>
-                      <Accordion.Body>
-                        <li>Pediatria: 4.8</li>
-                      </Accordion.Body>
-                    </Accordion.Item>
-                  </Accordion>
-                  <Accordion>
-                    <Accordion.Item eventKey="0">
-                      <Accordion.Header>Rotación 2
-                      </Accordion.Header>
-                      <Accordion.Body>
-                        <li>psicologia: 4.5</li>
-                      </Accordion.Body>
-                    </Accordion.Item>
-                  </Accordion>
-                </Accordion.Body>
-              </Accordion.Item>
-              <Accordion.Item eventKey="1">
-                <Accordion.Header>Juan Andres Perez</Accordion.Header>
-                <Accordion.Body>
-                  <li>Documento: 10000000</li>
-                  <li>semestre actual: 12</li>
-                  <li>Hospital actual: curitas del saber</li>
-                  <li>fecha inicio actual: 21/02/22</li>
-                  <li>Fecha finalización actual: 21/04/2022</li>
-                  <Accordion>
-                    <Accordion.Item eventKey="0">
-                      <Accordion.Header>Rotación 1
-                      </Accordion.Header>
-                      <Accordion.Body>
-                        <li>Pediatria: 4.8</li>
-                      </Accordion.Body>
-                    </Accordion.Item>
-                  </Accordion>
-                  <Accordion>
-                    <Accordion.Item eventKey="0">
-                      <Accordion.Header>Rotación 2
-                      </Accordion.Header>
-                      <Accordion.Body>
-                        <li>psicologia: 4.5</li>
-                      </Accordion.Body>
-                    </Accordion.Item>
-                  </Accordion>
-                </Accordion.Body>
-              </Accordion.Item>
-            </Accordion>
+            <Row>
+              <Col md={11} xs={10}>
+                <Accordion>
+                  <Accordion.Item eventKey="0">
+                    <Accordion.Header>
+                      Jennifer acosta
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <Row>
+                        <Col>
+                          <ul>
+                            <li>Documento: 10000000</li>
+                            <li>semestre actual: 11</li>
+                            <li>Hospital actual: curitas del saber</li>
+                            <li>fecha inicio actual: 21/02/22</li>
+                            <li>Fecha finalización actual: 21/04/2022</li>
+                          </ul>
+                        </Col>
+                        <Col>
+                          <Accordion>
+                            <Accordion.Item eventKey="0">
+                              <Accordion.Header>Rotación 1
+                              </Accordion.Header>
+                              <Accordion.Body>
+                                <li>Pediatria: 4.8</li>
+                              </Accordion.Body>
+                            </Accordion.Item>
+                          </Accordion>
+                          <Accordion>
+                            <Accordion.Item eventKey="0">
+                              <Accordion.Header>Rotación 2
+                              </Accordion.Header>
+                              <Accordion.Body>
+                                <li>psicologia: 4.5</li>
+                              </Accordion.Body>
+                            </Accordion.Item>
+                          </Accordion>
+                        </Col>
+                      </Row>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                  <Accordion.Item eventKey="1">
+                    <Accordion.Header>Juan Andres Perez</Accordion.Header>
+                    <Accordion.Body>
+                      <li>Documento: 10000000</li>
+                      <li>semestre actual: 12</li>
+                      <li>Hospital actual: curitas del saber</li>
+                      <li>fecha inicio actual: 21/02/22</li>
+                      <li>Fecha finalización actual: 21/04/2022</li>
+                      <Accordion>
+                        <Accordion.Item eventKey="0">
+                          <Accordion.Header>Rotación 1
+                          </Accordion.Header>
+                          <Accordion.Body>
+                            <li>Pediatria: 4.8</li>
+                          </Accordion.Body>
+                        </Accordion.Item>
+                      </Accordion>
+                      <Accordion>
+                        <Accordion.Item eventKey="0">
+                          <Accordion.Header>Rotación 2
+                          </Accordion.Header>
+                          <Accordion.Body>
+                            <li>psicologia: 4.5</li>
+                          </Accordion.Body>
+                        </Accordion.Item>
+                      </Accordion>
+                    </Accordion.Body>
+                  </Accordion.Item>
+                </Accordion>
+              </Col>
+              <Col className="p-1" xs={1}>
+                <ButtonGroup aria-label="Basic example">
+                  <Button variant="primary" onClick={() => console.log('waaad')}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-fill" viewBox="0 0 16 16">
+                      <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                    </svg>
+                  </Button>
+                  <Button variant="secondary">
+                    <input type="checkbox"></input>
+                  </Button>
+                </ButtonGroup>
+              </Col>
+            </Row>
           </FormGroup>
         </CardBody>
 
@@ -138,7 +163,7 @@ const Buttons = () => {
           {/* Card-3*/}
           {/* --------------------------------------------------------------------------------*/}
           <Card>
-            <CardTitle tag="h0" className="border-bottom p-0 mb-0">
+            <CardTitle tag="h1" className="border-bottom p-0 mb-0">
             </CardTitle>
             <CardBody className="">
               <div className="button-group">
