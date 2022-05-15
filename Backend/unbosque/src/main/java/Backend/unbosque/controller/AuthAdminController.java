af2e9374207e33904aa9820148cf7573fdfa44fe
sebas2208 <sebas2208@hotmail.com>
2022-05-14 19:12:56 -0500

    CrossOrigin

--- Backend/unbosque/src/main/java/Backend/unbosque/controller/AuthAdminController.java
+++ Backend/unbosque/src/main/java/Backend/unbosque/controller/AuthAdminController.java
@@ -3,6 +3,7 @@ package Backend.unbosque.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import Backend.unbosque.model.Admin;
 import Backend.unbosque.service.serviceApi.AdminService;
 
 @RestController
+@CrossOrigin(origins = {"https://localhost:3000", "https://mondsinc.vercel.app", "http://localhost:3000"})
 @RequestMapping({"/admin/auth"})
 public class AuthAdminController {
 
