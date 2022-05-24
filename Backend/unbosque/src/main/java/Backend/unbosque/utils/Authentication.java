8226edc3ca25a4aa5bf8fd676755570476e807b2
Daniela <dvictoria@unbosque.edu.co>
2022-05-24 14:00:11 -0500

    Test Login 7

--- Backend/unbosque/src/main/java/Backend/unbosque/utils/Authentication.java
+++ Backend/unbosque/src/main/java/Backend/unbosque/utils/Authentication.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
 
 import Backend.unbosque.model.Token;
 
+@Service
 public class Authentication {
     @Autowired
     private MongoOperations mongoOperations;
