e11ce9a9b0f00f39b79914938e250a5bf9d31786
sebas2208 <sebas2208@hotmail.com>
2022-06-06 09:39:38 -0500

    rotacion actualizacion

--- Backend/unbosque/src/main/java/Backend/unbosque/model/Rotacion.java
+++ Backend/unbosque/src/main/java/Backend/unbosque/model/Rotacion.java
@@ -1,6 +1,7 @@
 package Backend.unbosque.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
@@ -26,12 +27,18 @@ public class Rotacion {
         private double notaRotacion;
         private String mes;
         private LocalDate fechaInicio;
-        private LocalDate fechaCierre;  
+        private LocalDate fechaCierre;
+        private String observaciones;
+        private String evaluador1;
+        private List<String> firma1;
+        private List<String> firma2;
+        private String evaluador2;  
 
         public Rotacion(String idRotacion, Estudiante estudiante, Institucion institucion, Asignatura asignatura,
                 SubAsignatura subAsignatura, double notaHistoriaClinica, double notaResponsabilidad,
                 double notaPractica, double notaCyAC, double notaRotacion, String mes, LocalDate fechaInicio,
-                LocalDate fechaCierre) {
+                LocalDate fechaCierre, String observaciones, String evaluador1, List<String> firma1,
+                List<String> firma2, String evaluador2) {
             this.idRotacion = idRotacion;
             this.estudiante = estudiante;
             this.institucion = institucion;
@@ -45,6 +52,11 @@ public class Rotacion {
             this.mes = mes;
             this.fechaInicio = fechaInicio;
             this.fechaCierre = fechaCierre;
+            this.observaciones = observaciones;
+            this.evaluador1 = evaluador1;
+            this.firma1 = firma1;
+            this.firma2 = firma2;
+            this.evaluador2 = evaluador2;
         }
 
         public String getIdRotacion() {
@@ -151,12 +163,54 @@ public class Rotacion {
             this.asignatura = asignatura;
         }
 
+        public String getObservaciones() {
+            return observaciones;
+        }
+
+        public void setObservaciones(String observaciones) {
+            this.observaciones = observaciones;
+        }
+
+        public String getEvaluador1() {
+            return evaluador1;
+        }
+
+        public void setEvaluador1(String evaluador1) {
+            this.evaluador1 = evaluador1;
+        }
+
+        public List<String> getFirma1() {
+            return firma1;
+        }
+
+        public void setFirma1(List<String> firma1) {
+            this.firma1 = firma1;
+        }
+
+        public List<String> getFirma2() {
+            return firma2;
+        }
+
+        public void setFirma2(List<String> firma2) {
+            this.firma2 = firma2;
+        }
+
+        public String getEvaluador2() {
+            return evaluador2;
+        }
+
+        public void setEvaluador2(String evaluador2) {
+            this.evaluador2 = evaluador2;
+        }
+
         @Override
         public String toString() {
-            return "Rotacion [asignatura=" + asignatura + ", estudiante=" + estudiante + ", fechaCierre=" + fechaCierre
-                    + ", fechaInicio=" + fechaInicio + ", idRotacion=" + idRotacion + ", institucion=" + institucion
-                    + ", mes=" + mes + ", notaCyAC=" + notaCyAC + ", notaHistoriaClinica=" + notaHistoriaClinica
-                    + ", notaPractica=" + notaPractica + ", notaResponsabilidad=" + notaResponsabilidad
-                    + ", notaRotacion=" + notaRotacion + ", subAsignatura=" + subAsignatura + "]";
-        }
+            return "Rotacion [asignatura=" + asignatura + ", estudiante=" + estudiante + ", evaluador1=" + evaluador1
+                    + ", evaluador2=" + evaluador2 + ", fechaCierre=" + fechaCierre + ", fechaInicio=" + fechaInicio
+                    + ", firma1=" + firma1 + ", firma2=" + firma2 + ", idRotacion=" + idRotacion + ", institucion="
+                    + institucion + ", mes=" + mes + ", notaCyAC=" + notaCyAC + ", notaHistoriaClinica="
+                    + notaHistoriaClinica + ", notaPractica=" + notaPractica + ", notaResponsabilidad="
+                    + notaResponsabilidad + ", notaRotacion=" + notaRotacion + ", observaciones=" + observaciones
+                    + ", subAsignatura=" + subAsignatura + "]";
+        }   
 }
