fff48f0a393040a0ca469baab559b05a457d5cd5
Daniela <dvictoria@unbosque.edu.co>
2022-06-08 23:14:52 -0500

    juju

--- Backend/unbosque/src/main/java/Backend/unbosque/model/Email.java
+++ Backend/unbosque/src/main/java/Backend/unbosque/model/Email.java
@@ -1,12 +1,8 @@
 package Backend.unbosque.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 
 public class Email {
     private String recipient;
